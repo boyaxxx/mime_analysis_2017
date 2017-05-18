8e62da69544a029bc11feb805ef36c4fb913cc82
PythonCakeMing <1851464029@qq.com>
2017-05-18 17:34:51 +0800

    doctor

--- doctor/models.py
+++ doctor/models.py
@@ -2,14 +2,16 @@ from django.db import models
 
 
 # Create your models here.
-class AgeGroup(models.Model):
-    pass
+class AgeRange(models.Model):
+    range = models.CharField()
+    range_description = models.CharField()
+
 
 class Doctor(models.Model):
     phone = models.CharField(max_length=11, unique=True)
-    age = models.CharField()
     province = models.CharField()
     city = models.CharField()
+    age_range = models.ForeignKey(AgeRange)
     hospital_level = models.CharField()
     hospital_office = models.CharField()
 
@@ -30,7 +32,7 @@ class LoginStatistics(models.Model):
 
 class AuthorizeStatistics(models.Model):
     doctor = models.ForeignKey(Doctor)
-    login_year = models.IntegerField()
-    login_month = models.IntegerField()
-    login_day = models.IntegerField()
+    authorize_year = models.IntegerField()
+    authorize_month = models.IntegerField()
+    authorize_day = models.IntegerField()
 
