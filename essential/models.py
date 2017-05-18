009d5324c0e87c3eed56fe058f24f2109fea9899
PythonCakeMing <1851464029@qq.com>
2017-05-18 15:41:43 +0800

    essential

--- essential/models.py
+++ essential/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class TotalStatistics(models.Model):
+    register_count = models.IntegerField(default=0)
+    login_count = models.IntegerField(default=0)
+
+
+class DailyStatistics(models.Model):
+    year = models.IntegerField()
+    month = models.IntegerField()
+    day = models.IntegerField()
+
+    register_count = models.IntegerField(default=0)
+    login_count = models.IntegerField(default=0)
+
