23695e92912cadf9660afb64525507ebb928e976
PythonCakeMing <1851464029@qq.com>
2017-05-19 15:58:04 +0800

    essential

--- essential/models.py
+++ essential/models.py
@@ -2,15 +2,6 @@ from django.db import models
 
 
 # Create your models here.
-class TotalStatistics(models.Model):
-    visit_count = models.IntegerField(default=0)
-    register_count = models.IntegerField(default=0)
-    authorize_count = models.IntegerField(default=0)
-
-    order_count = models.IntegerField(default=0)
-    turnover_count = models.IntegerField(default=0)
-
-
 class DailyStatistics(models.Model):
     year = models.IntegerField()
     month = models.IntegerField()
@@ -24,21 +15,7 @@ class DailyStatistics(models.Model):
     turnover_count = models.IntegerField(default=0)
 
 
-class RegionStatistics(models.Model):
-    province = models.CharField()
-    city = models.CharField()
-    longitude = models.DecimalField(12, 6)
-    latitude = models.DecimalField(12, 6)
-
-    visit_count = models.IntegerField(default=0)
-    register_count = models.IntegerField(default=0)
-    authorize_count = models.IntegerField(default=0)
-
-    order_count = models.IntegerField(default=0)
-    turnover_count = models.IntegerField(default=0)
-
-
-class DailyProvinceStatistics(models.Model):
+class DailyRegionStatistics(models.Model):
     year = models.IntegerField()
     month = models.IntegerField()
     day = models.IntegerField()
