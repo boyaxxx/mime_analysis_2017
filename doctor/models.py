48c7917f02a0284cb42a0054a679d55e927b4ae8
PythonCakeMing <1851464029@qq.com>
2017-05-19 15:50:54 +0800

    doctor

--- doctor/models.py
+++ doctor/models.py
@@ -30,9 +30,26 @@ class Doctor(models.Model):
     doctor_title = models.ForeignKey(DoctorTitle)
     hospital_level = models.ForeignKey(HospitalLevel)
     hospital_office = models.ForeignKey(HospitalOffice)
+    province = models.CharField()
+    city = models.CharField()
+    longitude = models.DecimalField(12, 6)
+    latitude = models.DecimalField(12, 6)
+    register_year = models.CharField()
+    register_month = models.CharField()
+    register_day = models.CharField()
+    register_time = models.DateTimeField()
 
 
 class AgeRangeTotalStatistics(models.Model):
+    age_range = models.ForeignKey(AgeRange)
+    count = models.IntegerField(default=0)
+
+
+class AgeRangeDailyStatistics(models.Model):
+    year = models.IntegerField()
+    month = models.IntegerField()
+    day = models.IntegerField()
+
     age_range = models.ForeignKey(AgeRange)
     count = models.IntegerField(default=0)
 
@@ -42,11 +59,38 @@ class HospitalLevelTotalStatistics(models.Model):
     count = models.IntegerField(default=0)
 
 
+class HospitalLevelDailyStatistics(models.Model):
+    year = models.IntegerField()
+    month = models.IntegerField()
+    day = models.IntegerField()
+
+    hospital_level = models.ForeignKey(HospitalLevel)
+    count = models.IntegerField(default=0)
+
+
 class HospitalOfficeTotalStatistics(models.Model):
     hospital_office = models.ForeignKey(HospitalOffice)
     count = models.IntegerField(default=0)
 
 
+class HospitalOfficeDailyStatistics(models.Model):
+    year = models.IntegerField()
+    month = models.IntegerField()
+    day = models.IntegerField()
+
+    hospital_office = models.ForeignKey(HospitalOffice)
+    count = models.IntegerField(default=0)
+
+
 class DoctorTitleTotalStatistics(models.Model):
+    doctor_title = models.ForeignKey(DoctorTitle)
+    count = models.IntegerField(default=0)
+
+
+class DoctorTitleDailyStatistics(models.Model):
+    year = models.IntegerField()
+    month = models.IntegerField()
+    day = models.IntegerField()
+
     doctor_title = models.ForeignKey(DoctorTitle)
     count = models.IntegerField(default=0)
